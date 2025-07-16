5fe566ae0cc4696bbf0fbd590535b56a44a19f53
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-07-16 15:40:49 +0530

    Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Welcome to Naksh Jewels</h1>
+    </main>
+  );
+}
